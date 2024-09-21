ebcf720626cd83cedf9fe10accee04cecadd7b53
alext <alex112244@gmail.com>
2024-09-21 17:04:52 +0300

    added console logging

--- PlaygroundWeb/Program.cs
+++ PlaygroundWeb/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 var app = builder.Build();
 
@@ -21,7 +25,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", ([FromServices] ILogger<WeatherForecast> logger) =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
@@ -31,6 +35,9 @@ app.MapGet("/weatherforecast", () =>
                     summaries[Random.Shared.Next(summaries.Length)]
                 ))
             .ToArray();
+
+        logger.Log(LogLevel.Warning, "returned forecast {Date}", DateTime.UtcNow);
+
         return forecast;
     })
     .WithName("GetWeatherForecast")
